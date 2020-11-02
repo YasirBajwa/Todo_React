2bbf1420083ce4eabe72fc34019c4744c3a09920
YasirBajwa <yasirbajwa369@gmail.com>
2020-11-02 22:55:40 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component{
+    constructor(){
+        super();
+
+        this.state ={
+            todos: [{title:'Apple',edit:false},{title:'Banana',edit:false}],
+            value : ''
+        }
+    }
+
+    add_todo = () =>{
+        // first way using push method
+        // let obj = {title: this.state.value}
+        // this.state.todos.push(obj);
+        // this.setState({
+        //     todos: this.state.todos,
+        //     value : ''
+        // })
+
+        //second method using spread operator
+        let obj = {title: this.state.value}
+        this.setState({
+            todos: [...this.state.todos,obj],
+            value : ''
+        })
+
+    }
+
+    delete_todo = (index) =>{
+        this.state.todos.splice(index,1);
+        this.setState({
+            todos: this.state.todos
+        })
+    }
+    edit_todo = (index) => {
+        this.state.todos[index].edit = true;
+        this.setState({
+            todos:this.state.todos
+        })
+        
+    }
+    update_todo = (index) => {
+        this.state.todos[index].edit = false;
+        this.setState({
+            todos:this.state.todos
+        })
+        
+    }
+    handleChange = (e,index) => {
+        this.state.todos[index].title = e.target.value;
+        this.setState({
+            todos: this.state.todos
+        })
+    }
+    delte_all = () => {
+        // this.state.todos = '';
+        this.setState({
+            todos: []
+        })
+    }
+    render(){
+        let {todos,value} = this.state;
+        return(
+            <div>
+                <h1>Todo App</h1>
+                <input value={value} type='text' onChange={(e) => this.setState({value:e.target.value}) } placeholder='Enter Todo'/>
+                <button onClick={this.add_todo}>Add Todo</button>
+                <button onClick={this.delte_all}>Delete All</button>
+                <ul>
+                    {todos.map((item,index) => {
+                        return <li key={index}>
+                            {
+                                item.edit ? <input type='text' value={item.title} onChange={ (e) => this.handleChange(e,index)}/> : item.title
+
+                            }
+                            {
+                            item.edit ? <button onClick={() => this.update_todo(index)}>Update</button> :
+                            <button onClick={() => this.edit_todo(index)}>Edit</button> 
+                            }
+                                
+
+                            <button onClick={() => this.delete_todo(index)}>Delete</button>
+
+                            </li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
 }
+
 
 export default App;
